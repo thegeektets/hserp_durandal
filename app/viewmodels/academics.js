80df31eca230468e0e1682b03bd609d8b7b2cc56
thegeektets <griffinmuteti31@gmail.com>
2014-08-30 16:24:27 +0300

    logo changes

--- app/viewmodels/academics.js
+++ app/viewmodels/academics.js
@@ -2,20 +2,8 @@
     var ctor = function () {
         this.viewtitle = 'Academics';
         this.displayName = 'Academics';
-        this.description = 'Academics is a cross-device, cross-platform client framework written in JavaScript and designed to make Single Page Applications (SPAs) easy to create and maintain.';
-        this.features = [
-            'Clean MV* Architecture',
-            'JS & HTML Modularity',
-            'Simple App Lifecycle',
-            'Eventing, Modals, Message Boxes, etc.',
-            'Navigation & Screen State Management',
-            'Consistent Async Programming w/ Promises',
-            'App Bundling and Optimization',
-            'Use any Backend Technology',
-            'Built on top of jQuery, Knockout & RequireJS',
-            'Integrates with other libraries such as SammyJS & Bootstrap',
-            'Make jQuery & Bootstrap widgets templatable and bindable (or build your own widgets).'
-        ];
+        this.description = '';
+        this.features = [];
         this.search = function() {
             //It's really easy to show a message box.
             //You can add custom options too. Also, it returns a promise for the user's response.
